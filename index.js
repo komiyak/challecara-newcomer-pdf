190ce630f7c1bee66552bf25c52e2493b81abe0b
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-07-09 16:47:40 +0900

    Add multi page settings.

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ const schoolName = 'Fukuoka College'
 const studentName = '田中 太郎'
 const qrCodeData = 'https://example.com/'
 
-var docDefinition = {
-  content: [
+const content = []
+
+for (let i = 0; i < 5; i++) {
+  const pageContent = [
     { image: 'images/logo_border.png', width: 260 },
     { text: `会場: ${locationName}`, fontSize: 20 },
     { text: `\n所属: ${schoolName}`, fontSize: 26 },
@@ -28,8 +30,15 @@ var docDefinition = {
     'チャレキャラを始めるためのツール等のセットアップを行います。\n\n\n',
     { qr: qrCodeData, fit: 140, alignment: 'center' },
     '\n\n・これはあなた専用の QR コードなので、他人と共有しないでください',
-    '・セットアップ完了後は不要になりますので、破棄してください。'
-  ],
+    '・セットアップ完了後は不要になりますので、破棄してください。',
+    { text: '', pageBreak: 'after' }
+  ]
+
+  content.push(pageContent)
+}
+
+var docDefinition = {
+  content: [].concat.apply([], content),
   defaultStyle: {
     font: 'IpaMincho'
   }
