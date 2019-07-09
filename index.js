78c79611fe5a469f2f2d138f7fb6ef5cb3fcf7d8
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-07-10 08:00:36 +0900

    Install the firebase-admin.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+require('dotenv').config()
+
+const admin = require('firebase-admin')
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+})
+
+const db = admin.firestore()
+
 const fonts = {
   IpaMincho: {
     normal: 'fonts/ipam.ttf',
